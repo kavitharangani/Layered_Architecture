f0ba663f9a9c10f578cbb6dc0981f8384f69d444
kavitharangani <tharanganikavi08@gmail.com>
2023-05-26 08:27:37 +0530

    change

--- src/dao/OrderDetailsDAOImpl.java
+++ src/dao/OrderDetailsDAOImpl.java
@@ -2,17 +2,70 @@ package dao;
 
 import db.DBConnection;
 import model.CustomerDTO;
+import model.OrderDTO;
 import model.OrderDetailDTO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class OrderDetailsDAOImpl implements OrderDetailDAO{
+public class OrderDetailsDAOImpl implements CRUDDAO<OrderDetailDTO,String>{
     @Override
-    public boolean saveOrderDetails(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+    public ArrayList<OrderDetailDTO> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean add(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public OrderDetailDTO search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
         String sql = SQLUtil.execute("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
         return SQLUtil.execute(sql, dto.getOid(), dto.getItemCode(), dto.getUnitPrice(), dto.getQty());
     }
+    @Override
+    public String generateNewCode() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public String generateOID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+
+  /*  @Override
+    public boolean saveOrderDetails(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+        String sql = SQLUtil.execute("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
+        return SQLUtil.execute(sql, dto.getOid(), dto.getItemCode(), dto.getUnitPrice(), dto.getQty());
+    }*/
 
 }
